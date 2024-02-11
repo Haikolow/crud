43252e672d53c83de3f15015a11981754a321a6f
Artem <yariki2009@yandex.ru>
2024-02-11 14:35:48 +0300

    Pagination

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': '',
-        'PASSWORD': ''
+        'PASSWORD': '',
     }
 }
 
@@ -130,3 +130,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1
+}
